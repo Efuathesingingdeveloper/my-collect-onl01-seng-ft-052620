b24f0eedc67e7ab896932aac3390017b609f4061
Georgette Class-Peters <georgycp@udel.edu>
2020-06-03 17:49:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,10 +6,10 @@ modified_array =[]
   
 while index < array.length
 modified_array << yield(array[index])
-index += 1
+  index += 1
 end
 
-
+modified_array
 end
 
 #array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
